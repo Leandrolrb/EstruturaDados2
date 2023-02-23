f4b6528989000b3883c17c29e2fa5fff2c7ce2b2
João Paulo Preti <jppreti@gmail.com>
2023-02-23 16:30:39 -0400

    feat: Lista Ligada

---
+++ LinkedListTest.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "log.h"
+
+int main() {
+	FILE *file = fopen("program.log","w");
+	
+	log_set_level(LOG_INFO);
+	log_add_fp(file, LOG_TRACE);
+	
+	LinkedList lista;
+	init(&lista);
+
+	int *numero = malloc(sizeof(int));
+	*numero = 10;
+	enqueue(&lista, numero);
+	numero = malloc(sizeof(int));
+	*numero = 20;
+	enqueue(&lista, numero);
+				
+	return EXIT_SUCCESS;
+}
